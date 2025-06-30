50ffa787913719372c936eed71362e42e86ca93e
Sajith Bandara <140834328+sajith23420@users.noreply.github.com>
2025-07-01 00:01:12 +0530

    Frontend UI design done

---
+++ src/config/gemini.js
@@ -0,0 +1,44 @@
+
+
+
+// See https://developers.google.com/apps-script/guides/properties
+// for instructions on how to set the script properties.
+const apiKey = PropertiesService.getScriptProperties().getProperty('AIzaSyBuwlsGvJGe_us6dZ_18C4jc1STUWCQhC4');
+const model = 'gemini-2.5-pro';
+const api = 'streamGenerateContent';
+const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:${api}?key=${apiKey}`;
+
+function main() {
+    const payload = {
+        contents: [
+            {
+                role: 'user',
+                parts: [
+                    {
+                        text: `INSERT_INPUT_HERE`
+                    },
+                ]
+            },
+        ],
+        generationConfig: {
+            thinkingConfig: {
+                thinkingBudget: -1,
+            },
+            responseMimeType: 'text/plain',
+        },
+    };
+
+    const options = {
+        method: 'POST',
+        contentType: 'application/json',
+        muteHttpExceptions: true,
+        payload: JSON.stringify(payload),
+    };
+
+    const response = UrlFetchApp.fetch(url, options);
+    const chunks = JSON.parse(response.getContentText());
+
+    for (const chunk of chunks) {
+        console.log(chunk.candidates[0].content.parts[0].text);
+    }
+}
